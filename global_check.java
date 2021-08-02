9bc404af6db9e3cf2dc52c38bbd3b578b493f95c
v370r <50656923+v370r@users.noreply.github.com>
2021-08-02 19:16:29 +0530

    Add files via upload

---
+++ global_check.java
@@ -0,0 +1,17 @@
+package doubts;
+
+public class global_check{
+    // int x;  //throws error
+    // int y;
+    // x =y;
+    // y =10;
+    int x=5;
+    public static void main(String []args){
+        global_check e = new global_check();
+        System.out.println(e.x);
+     }
+    void bar(){
+     int c= w;
+     }
+    int w = 5;// wtf howww
+}
---
+++ globaldoubt.java
@@ -0,0 +1,16 @@
+package doubts;
+
+public class globaldoubt {
+    public static void main(String []args){
+        globaldoubt e = new globaldoubt();
+        e.bar();
+        }
+    void bar(){
+        int c= w;
+        System.out.println(c);
+        } 
+    int w = 5;// wtf howww did it pick and not the next one?
+    // this thrws error int x=y,y= 0; 
+
+}
+    
